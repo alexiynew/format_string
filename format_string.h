d48974dd7bc6359f578d5ab7aa2fd12136d82c6b
alexiynew <lehale@list.ru>
2015-07-01 21:51:15 +0300

    add automatic fiekd indexing support

--- format_string.h
+++ format_string.h
@@ -6,17 +6,22 @@
 #include <memory>
 #include <cctype>
 
+
+// forvard declaration
+// Here should be description
 template <typename... Args>
 std::string make_string (const std::string& str, Args&&... args);
 
-namespace {
-
+	
 class formater_base {
 protected:
 	formater_base() {}
 	virtual ~formater_base() {}
-	virtual void print_index(std::istream& in, std::ostream& out) const = 0;
+	virtual void print_value(std::istream& in, std::ostream& out) = 0;
 
+	// Internal types
+	enum class indexing_mode { none, automatic, manual };
+	
 	struct value_base {
 		virtual ~value_base(){};
 		virtual void print_to(std::ostream& os) const = 0;
@@ -28,14 +33,15 @@ protected:
 		void print_to(std::ostream& os) const override { os << value; }
 		const T & value;
 	};
-
+	
+	// Helper function
 	template <typename T>
 	value_holder<T>* make_value_holder(const T& t)
 	{
 		return new value_holder<T>(t);
 	}
 
-	std::string parse(const std::string& str) const
+	std::string parse(const std::string& str)
 	{
 		std::istringstream in(str);
 		std::ostringstream out;
@@ -49,10 +55,12 @@ protected:
 					continue; 
 				}
 				
-				print_index(in, out);
+				print_value(in, out);
 	
 				in.get(c);
-				if (c != '}') { throw std::logic_error("'}' expected"); }
+				if (c != '}') { 
+					throw std::logic_error("'}' expected"); 
+				}
 			} else if (c == '}') {
 				if (in.peek() == '}') { 
 					out << '}'; 
@@ -65,32 +73,54 @@ protected:
 			}
 		}
 		return out.str();
-	} 
+	}
 };
 
 template <size_t N>
 class formater : private formater_base {
-
+	// format data
+	std::unique_ptr<value_base> values[N];
+	formater_base::indexing_mode mode;
+	size_t next_index;
+	
+	// constructor
 	template <typename... Args>
-	formater(Args&&... args) : values{std::unique_ptr<value_base>(make_value_holder(args))...,}
+	formater(Args&&... args) 
+		: values{std::unique_ptr<value_base>(make_value_holder(args))...,}, mode(indexing_mode::none), next_index(0)
 	{}
-
-	void print_index(std::istream& in, std::ostream& out) const override
+	
+	void print_value(std::istream& in, std::ostream& out) override
 	{
 		size_t index; 
-		if (!std::isdigit(in.peek())) { throw std::logic_error("Index number expected"); }
-		in >> index;
-		if (index >= N) { throw std::logic_error("Wrong index"); }
+		
+		if (in.peek() == '}') {
+			if (mode == indexing_mode::manual) { 
+				throw std::logic_error("You can not switch from manual fild indexing mode to automatic"); 
+			}
+			mode = indexing_mode::automatic;
+			index = next_index++;
+		} else if (std::isdigit(in.peek())) {
+			if (mode == indexing_mode::automatic) { 
+				throw std::logic_error("You can not switch from automatic fild indexing mode to manual"); 
+			}
+			mode = indexing_mode::manual;
+			in >> index;
+		} else 	{ 
+			throw std::logic_error("Index number expected"); 
+		}
+		
+		if (index >= N) {
+			throw std::logic_error("Wrong index"); 
+		}
+		
 		values[index]->print_to(out);
 	}
 
-	std::unique_ptr<value_base> values[N];
-
+	// give access to private methods
 	template <typename... Args>
-	friend std::string ::make_string (const std::string& str, Args&&... args);
+	friend std::string make_string (const std::string& str, Args&&... args);
 };
 
-}; 
 
 
 template <typename... Args>
--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 int main ()
 {
 	std::cout << make_string("one {0} two {1} three {2}", 1, 0.42, "string") << std::endl;
+	std::cout << make_string("one {} two {} three {}", 1, 0.42, "string") << std::endl;
 	std::cout << make_string("one {0} two {0} three {0}", "string") << std::endl;
 	std::cout << make_string("one {{{0}}}", "string") << std::endl;
 }
